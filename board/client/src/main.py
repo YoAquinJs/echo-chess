a98cfe2e2d793051463a54931e8b993de13159d9
YoAquinjs <sebascertuche@gmail.com>
2024-11-23 09:07:24 -0600

    add dotenv conf and update pyproject for package mode

--- board/client/src/main.py
+++ board/client/src/main.py
@@ -4,9 +4,13 @@ Voice-Chess Client
 Configure via .env file in src/
 """
 
+from dotenv import load_dotenv
+
 
 def main():
     """program entry point"""
+
+    load_dotenv()
 
 
 if __name__ == "__main__":
